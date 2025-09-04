71901e3af917062f4b73924e0e9fbe766e2d4380
elsenhans <48911833+elsenhans@users.noreply.github.com>
2025-09-04 15:58:27 +0200

    add basic files and structure (#1)
    * add-basic-files-and-structure
    
    * add-basic-files-and-structure
    
    * add-basic-files-and-structure
    
    ---------
    
    Co-authored-by: elsenhans <elsenhans@users.noreply.github.com>

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const config = require('@folio/jest-config-stripes');
+
+const esModules = [
+  '@folio',
+  'ky',
+].join('|');
+
+module.exports = {
+  ...config,
+  setupFiles: [
+    ...config.setupFiles,
+    path.join(__dirname, './test/jest/setupFiles.js'),
+  ],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+};
