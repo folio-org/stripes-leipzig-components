54a3164a49a20b060208f822288bbabbcc08711e
elsenhans <elsenhans@users.noreply.github.com>
2025-10-01 13:22:41 +0200

    add-EditCard-component

---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as EditCard } from './lib/EditCard';
---
+++ test/jest/helpers/renderWithIntlConfiguration.js
@@ -0,0 +1,41 @@
+import { IntlProvider } from 'react-intl';
+
+import { render } from '@folio/jest-config-stripes/testing-library/react';
+import stripesComponentsTranslations from '@folio/stripes-components/translations/stripes-components/en';
+import stripesSmartComponentsTranslations from '@folio/stripes-smart-components/translations/stripes-smart-components/en';
+
+import localTranslations from '../../../translations/stripes-leipzig-components/en';
+
+const translationSets = [
+  {
+    prefix: 'stripes-leipzig-components',
+    translations: localTranslations,
+  },
+  {
+    prefix: 'stripes-components',
+    translations: stripesComponentsTranslations,
+  },
+  {
+    prefix: 'stripes-smart-components',
+    translations: stripesSmartComponentsTranslations,
+  },
+];
+
+function renderWithIntlConfiguration(children, locale = 'en') {
+  const allTranslations = {};
+
+  translationSets.forEach((set) => {
+    const { prefix, translations } = set;
+    Object.keys(translations).forEach(key => {
+      allTranslations[`${prefix}.${key}`] = translations[key];
+    });
+  });
+
+  return render(
+    <IntlProvider locale={locale} messages={allTranslations}>
+      {children}
+    </IntlProvider>
+  );
+}
+
+export default renderWithIntlConfiguration;
